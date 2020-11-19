c36f05d2a791ec6753893d37c5e187e49f2c13fe
akbarjane <501123445a@gmail.com>
2020-11-19 17:58:44 +0500

    init

---
+++ Create.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OfficeOpenXml;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace Excel
+{
+    public class Create 
+    {
+        public byte[] ExcelResult(Action<ExcelPackage> result)
+        {
+            var package = new ExcelPackage();
+            result(package); 
+            ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
+            return package.GetAsByteArray();
+        }
+
+        public void Build(Action<ExcelPackage> result, string path)
+        {
+            //Build new Excel file
+            var package = new ExcelPackage();
+            result(package);
+            ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
+            package.SaveAs( path switch {
+                "." => new FileInfo($@"{Directory.GetCurrentDirectory()}\\worksheet.xlsx".Replace("\'", "\\'")),
+                _ => new FileInfo($@"{path}.xlsx".Replace("\'", "\\'"))
+            });
+        }
+
+
+        
+    }
+
+    public static class ExcelResult {
+        public static FileContentResult Excel(this ControllerBase obj, Action<ExcelPackage> result,string fileName = "file"){
+            var package = new ExcelPackage();
+            result(package);
+            return obj.File(
+                fileContents: package.GetAsByteArray(),
+                contentType: "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+                fileDownloadName: fileName + ".xlsx");
+        }
+
+        public static WorkSheet addWorkSheet(this ExcelPackage obj, string worksheetName){
+            return new WorkSheet(obj,worksheetName);
+        }
+    }
+}
---
+++ Open.cs
@@ -0,0 +1,81 @@
+﻿using OfficeOpenXml;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using static Newtonsoft.Json.JsonConvert;
+
+namespace Excel
+{
+    public class Open
+    {
+        private string fileName;
+        public bool conteinHeaders = false;
+        public string ExceptionMessege = "Incorrect signature of Excel data";
+
+        public Open(string fileName, bool isCurrentDirectory = true) => this.fileName = isCurrentDirectory ? $"{Directory.GetCurrentDirectory()}\\" + fileName : fileName;
+
+        public string this[int row, int column]
+        {
+            get
+            {
+                byte[] bin = File.ReadAllBytes(fileName);
+                using (MemoryStream stream = new MemoryStream(bin))
+                {
+                    using (ExcelPackage excelPackage = new ExcelPackage(stream))
+                    {
+                        ExcelWorksheet obj = excelPackage.Workbook.Worksheets[0];
+                        return obj.Cells[row, column].Value.ToString();
+                    }
+                }
+            }
+        }
+        public dynamic ParseRow<T>()
+        {
+            byte[] bin = File.ReadAllBytes(fileName);
+            using MemoryStream stream = new MemoryStream(bin);
+            using ExcelPackage excelPackage = new ExcelPackage(stream);
+            ExcelWorksheet obj = excelPackage.Workbook.Worksheets[0];
+            return this.Parse<T>(obj, 2);
+        }
+
+        public List<T> ParseTo<T>(int row = 0)
+        {
+            byte[] bin = File.ReadAllBytes(fileName);
+            using MemoryStream stream = new MemoryStream(bin);
+            using ExcelPackage excelPackage = new ExcelPackage(stream);
+            ExcelWorksheet obj = excelPackage.Workbook.Worksheets[0];
+            var list = new List<T>();
+            var i = row;
+            while (true)
+            {
+                try
+                {
+                    i++;
+                    list.Add(this.Parse<T>(obj, i));
+                }
+                catch (System.Exception)
+                {
+                    break;
+                }
+
+            }
+            return list;
+
+        }
+
+        private dynamic Parse<T>(ExcelWorksheet reader, int row)
+        {
+            string typeString = @"{";
+            var props = typeof(T).GetProperties();
+            for (int i = 1; i < props.Count(); i++)
+            {
+                typeString += $" '{props[i - 1].Name}': '{reader.Cells[row, i].Value}' ,";
+            }
+            typeString += "}";
+            return DeserializeObject<T>(typeString);
+        }
+
+
+    }
+}
